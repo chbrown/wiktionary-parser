5f2a8a25606ea655918c823496e4de4ed1fb88d4
Christopher Brown <io@henrian.com>
2013-05-16 18:29:40 -0500

    Avoid underscores when possible. Namespaces---let's have more of those.

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name='wiktionary',
+    version='0.0.2',
+    author='Christopher Brown',
+    author_email='chrisbrown@utexas.edu',
+    packages=['wiktionary'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'wikitools'
+    ],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
